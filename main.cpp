1e9b81c6c5b945ed7a2386967195f744d9027a53
Kevin Holst <kevin.holst14@gmail.com>
2020-03-12 18:25:44 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,35 +11,47 @@ int main()
 	Grid sudoku;
 	sudoku.loadSolutions(solutions);
 	const int BEEP_LENGTH = 1250;
+	int choice = 1;
 	
-	int choice = -1;
 	while(true)
 	{
-		while (choice < 0 || choice > 4)
+		cout << "Number of solutions found thus far: " << solutions.size() << endl << endl;
+		cout << "[0] Quit program" << endl;
+		cout << "[1] Display solutions found" << endl;
+		cout << "[2] Generate solution" << endl;
+		cout << "[3] Generate many solutions (infinite)" << endl;
+		cout << "[4] Save solutions" << endl;
+		cin >> choice;
+		system("cls");
+
+		if (choice == 0)
 		{
-			cout << "Number of solutions found thus far: " << solutions.size() << endl << endl;
-			cout << "[0] Quit program" << endl;
-			cout << "[1] Display solutions found" << endl;
-			cout << "[2] Generate solution" << endl;
-			cout << "[3] Generate many solutions (infinite)" << endl;
-			cout << "[4] Save solutions" << endl;
-			cin >> choice;
-			system("cls");
-			
-			if (choice == 0)
+			break;
+		}
+		else if (choice == 1)
+		{
+			sudoku.loadSolutions(solutions);
+			for (int i = 0; i < solutions.size(); ++i)
 			{
-				return 0;
+				cout << i + 1 << ")" << endl;
+				solutions[i].printGrid();
 			}
-			else if (choice == 1)
+		}
+		else if (choice == 2)
+		{
+			while (sudoku.generateGrid() == false)
 			{
-				sudoku.loadSolutions(solutions);
-				for (int i = 0; i < solutions.size(); ++i)
-				{
-					cout << i + 1 << ")" << endl;
-					solutions.at(i).printGrid();
-				}
+				continue;
 			}
-			else if (choice == 2)
+			sudoku.printGrid();
+			sudoku.storeSolution(solutions);
+			sudoku.saveSolutions(solutions);
+			sudoku.loadSolutions(solutions);
+			Beep(523, BEEP_LENGTH);
+		}
+		else if (choice == 3)
+		{
+			while (true)
 			{
 				while (sudoku.generateGrid() == false)
 				{
@@ -51,27 +63,11 @@ int main()
 				sudoku.loadSolutions(solutions);
 				Beep(523, BEEP_LENGTH);
 			}
-			else if (choice == 3)
-			{
-				while (true)
-				{
-					while (sudoku.generateGrid() == false)
-					{
-						continue;
-					}
-					sudoku.printGrid();
-					sudoku.storeSolution(solutions);
-					sudoku.saveSolutions(solutions);
-					sudoku.loadSolutions(solutions);
-					Beep(523, BEEP_LENGTH);
-				}
-			}
-			else if (choice == 4)
-			{
-				sudoku.saveSolutions(solutions);
-			}
 		}
-		choice = -1;
+		else if (choice == 4)
+		{
+			sudoku.saveSolutions(solutions);
+		}
 	}
 	
 	return 0;
